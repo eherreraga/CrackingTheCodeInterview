5ada9c580bb5bf9e9bdb3e9f23d2122a050b0087
eherreraga <eherrerag.eh@gmail.com>
2018-04-22 21:26:50 -0600

    updated

--- Chapter3.java
+++ Chapter3.java
@@ -20,6 +20,22 @@ public class Chapter3{
 
     }
 
+    /**Problem 3.4
+     * Queue via Stacks: Implement a MyQueue class which implements a queue using two stacks.
+     */
+    public MyStack queueUsingStack(MyStack stack){
+        MyStack newStack = new MyStack<>();
+        newStack.push(stack.pop());
+        return newStack;
+    }
+
+    /**Problem 3.6
+     * Animal Shelter: An animal shelter, which holds only dogs and cats, operates on a strictly" rst in,  rst out" basis.
+     *  People must adopt either the"oldest" (based on arrival time) of all animals at the shelter,
+     *  or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of that type).
+     *  They cannot select which speci c animal they would like. Create the data structures to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog, and dequeueCat.
+     *  You may use the built-in Linked list data structure.
+     */
     public Animal dequeueDog(){
         Queue sQueue = new PriorityQueue<>();
         Animal dogAdopted;
@@ -32,6 +48,13 @@ public class Chapter3{
         return dogAdopted;
     }
 
+    /**Problem 3.6
+     * Animal Shelter: An animal shelter, which holds only dogs and cats, operates on a strictly" rst in,  rst out" basis.
+     *  People must adopt either the"oldest" (based on arrival time) of all animals at the shelter,
+     *  or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of that type).
+     *  They cannot select which speci c animal they would like. Create the data structures to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog, and dequeueCat.
+     *  You may use the built-in Linked list data structure.
+     */
     public Animal dequeueCat(){
         Queue sQueue = new PriorityQueue<>();
         Animal catAdopted;
@@ -42,8 +65,7 @@ public class Chapter3{
             shelterQueue = sQueue;
         }
         return catAdopted;
-    }
-    public class Animal{
+    }   public class Animal{
         String animalType;
         public Animal(String type){
             animalType = type;
